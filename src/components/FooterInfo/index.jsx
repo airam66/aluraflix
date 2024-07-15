449137435c2cfca4c5f32220e220f6a02574fdcb
airam66 <fairam66@gmail.com>
2024-07-15 01:20:16 -0300

    agregar enlace en el footer

--- src/components/FooterInfo/index.jsx
+++ src/components/FooterInfo/index.jsx
@@ -3,7 +3,7 @@ import "./FooterInfo.css"
 function FooterInfo() {
     return <footer className="footer-info-container">
     
-       <p>Desarollado por <a href="">airam66</a></p>
+       <p>Desarollado por <a href="https://github.com/airam66/aluraflix">airam66</a></p>
        <p>Challenge React - Alura Latam</p>
 
     </footer>
